9068a27cc64963eaa308285d669e29b17f2cd73f
Ricardo1335 <59890024+Ricardo1335@users.noreply.github.com>
2021-03-06 09:15:31 -0500

    mejora código 1

--- src/main/Java/Guesser/Adivinador.java
+++ src/main/Java/Guesser/Adivinador.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.RequestScoped;
+
 
 @ManagedBean(name = "guessBean")
 @ApplicationScoped
